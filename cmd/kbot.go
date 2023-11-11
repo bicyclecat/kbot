5068a782812c9f4a9f51e9b1cdb135025c5263f6
bicyclecat <edelweissman@yahoo.com>
2023-11-11 11:56:32 +0200

    kbot v1.0.2

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -46,6 +46,12 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 
 			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
+			}
 
 			return err
 
